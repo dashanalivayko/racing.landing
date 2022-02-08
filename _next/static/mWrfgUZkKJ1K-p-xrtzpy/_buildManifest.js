9e4782b873784f24a0c938a1c1f5ade3c179566f
dashanalivayko <dashanalivayko@users.noreply.github.com>
2022-02-08 19:22:10 +0000

    deploy: 6565d8f0b9806c477eda75a35f5f8819815de579

--- _next/static/mWrfgUZkKJ1K-p-xrtzpy/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/702-6beaa9b01ea99d6c.js","static/chunks/pages/index-e1bdedbbf036497c.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
