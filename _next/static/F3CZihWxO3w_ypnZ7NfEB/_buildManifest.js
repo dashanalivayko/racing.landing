d4610fca63dc07cdfd4db164cea016df62e1d5b2
NorthernShady <NorthernShady@users.noreply.github.com>
2022-02-08 13:26:52 +0000

    Deploying to gh-pages from @ FunnchatCompany/racing.landing@4f627a086a1eaaf02374172094ce0c810073a0a7 🚀

---
+++ _next/static/F3CZihWxO3w_ypnZ7NfEB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/702-32c145b6c837540d.js","static/chunks/pages/index-c6c222e53d8a3d45.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
