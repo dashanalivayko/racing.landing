e1ccbed3f5d85cd62dad33e6381c3c9db972704a
Darya Nalivayko <dashanalivayko@gmail.com>
2022-02-22 11:31:06 +0300

    fix

--- pages/game.tsx
+++ pages/game.tsx
@@ -1,6 +1,6 @@
 import { GameStyles } from '../styles/game.style';
 import React from 'react';
-import { UnityPage } from '../components/UnityPage';
+import { UnityPage } from '../components/UnityPage2';
 
 const Game: React.FC = () => {
   const styles = GameStyles();
